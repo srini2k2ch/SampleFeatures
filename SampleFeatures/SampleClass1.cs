8b50d3b857c7eb9630e74280e276ecdbd41e2dc3
srinivas.chilakalapudi <srinivas.chilakalapudi@prophecyunlimited.net>
2020-05-23 17:12:12 +0100

    Add project files.

---
+++ SampleFeatures/SampleClass1.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SampleFeatures
+{
+    class SampleClass1
+    {
+        public void Sample()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.youtube.com");
+            driver.FindElement(By.Name("search_query")).SendKeys("c# automation");
+            Thread.Sleep(3000);
+            driver.Quit();
+
+        }
+    }
+}
